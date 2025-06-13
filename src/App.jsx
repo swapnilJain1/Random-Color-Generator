3c267cd5d58d7ae7b4ceeca52240bb18c6ef0443
iamsonu1904 <iamsonu1904@gmail.com>
2025-06-13 07:36:56 +0000

    fixed formatting issues

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,25 @@ import { randomHexGenerator } from "./helper";
 function App() {
   const [hex, setHex] = useState("#843581");
   const [isCopied, setIsCopied] = useState(false);
- 
 
   const handleClick = () => {
     const finalRes = randomHexGenerator();
     setHex(finalRes);
   };
-const handleCopy = () => {
-  navigator.clipboard.writeText(hex);
-  setIsCopied(true);
-  setTimeout(() => {
-    setIsCopied(false);
-  }, 2000);
-}
-  
+  const handleCopy = () => {
+    navigator.clipboard.writeText(hex);
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 2000);
+  };
+
   return (
     <div className="container" style={{ backgroundColor: hex }}>
       {isCopied && <p className="copied">Copied to Ciplboard</p>}
-      <p onClick={handleCopy}>Hex Color code is : <span className="hex-code">{hex}</span></p>
+      <p onClick={handleCopy}>
+        Hex Color code is : <span className="hex-code">{hex}</span>
+      </p>
       <button onClick={handleClick}>Change Color</button>
     </div>
   );
