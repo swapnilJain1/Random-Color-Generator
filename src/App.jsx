81da560f685288d08dd6c0e4c622af29074227b7
iamsonu1904 <iamsonu1904@gmail.com>
2025-06-13 07:35:18 +0000

    Modified css and added clipboard copy functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
-import { useState } from 'react'
-import './App.css'
-import { randomHexGenerator } from './helper'
+import { useState } from "react";
+import "./App.css";
+import { randomHexGenerator } from "./helper";
 
 function App() {
-  const [hex, setHex] = useState("#843581")
+  const [hex, setHex] = useState("#843581");
+  const [isCopied, setIsCopied] = useState(false);
+ 
 
-
-
-const handleClick = ()=>{
- const finalRes = randomHexGenerator()
-setHex(finalRes)
+  const handleClick = () => {
+    const finalRes = randomHexGenerator();
+    setHex(finalRes);
+  };
+const handleCopy = () => {
+  navigator.clipboard.writeText(hex);
+  setIsCopied(true);
+  setTimeout(() => {
+    setIsCopied(false);
+  }, 2000);
 }
-
+  
   return (
-    <div className='container' style={{backgroundColor:hex}} >
-        {hex}
-        <button onClick={handleClick} >Change Color</button>
+    <div className="container" style={{ backgroundColor: hex }}>
+      {isCopied && <p className="copied">Copied to Ciplboard</p>}
+      <p onClick={handleCopy}>Hex Color code is : <span className="hex-code">{hex}</span></p>
+      <button onClick={handleClick}>Change Color</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/helper.js
+++ src/helper.js
@@ -1,13 +1,12 @@
 export const randomHexGenerator = () => {
-    const aphabet = "ABCDEF"
-    const numbers = "1234567890"
+  const aphabet = "ABCDEF";
+  const numbers = "1234567890";
 
-    let finalRes = "#"
-    const totalStr = aphabet + numbers
-    for (let i = 0; i < 6; i++) {
-        const randomHex = Math.floor(Math.random() * 16)
-        finalRes += totalStr[randomHex]
-    }
-    return finalRes
-
-} 
+  let finalRes = "#";
+  const totalStr = aphabet + numbers;
+  for (let i = 0; i < 6; i++) {
+    const randomHex = Math.floor(Math.random() * 16);
+    finalRes += totalStr[randomHex];
+  }
+  return finalRes;
+};
